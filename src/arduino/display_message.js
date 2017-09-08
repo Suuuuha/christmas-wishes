a4105e8fe87b12dde2f8235b8b7d871d8b0234f9
Matt Lubel <matt_lubel@hotmail.com>
2017-09-08 22:30:52 +0300

    refactor arduino

--- src/arduino/display_message.js
+++ src/arduino/display_message.js
@@ -1,32 +1,15 @@
-var five = require("johnny-five")
+const displayMessage = display => message => displayLetters(display)(
+  message.split(""),
+  () => console.log(`${message} finished printing.`)
+)
 
-var board = new five.Board({
-  repl: false
-})
-
-board.on("ready", function() {
-  console.log('board ready')
-  var display = new five.Led.Matrix({
-    pins: {
-      data: 2,
-      clock: 3,
-      cs: 4
-    }
-  })
-  display.on()
-})
-
-function displayMessage(display, message) {
-  displayLetters(display, message.split(""))
-}
-
-function displayLetters(display, letters) {
+const displayLetters = display => (letters, cb) => {
   if (!letters.length) {
     display.clear(0)
-    return console.log('done.')
+    return cb()
   }
   display.draw(0, letters[0])
-  setTimeout(() => displayLetters(display, letters.slice(1)), 300)
+  setTimeout(() => displayLetters(display)(letters.slice(1), cb), 300)
 }
 
 module.exports = displayMessage
